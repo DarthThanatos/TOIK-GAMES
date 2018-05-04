b9f1e919de772f1b47c01783a8bd2058452f3301
DarthThanatos <robert.bielas@interia.pl>
2018-05-04 17:06:38 +0200

    init

---
+++ angular_sudoku-gh-pages/apps/js/filters.js
@@ -0,0 +1,36 @@
+Sudoku.filter('column_modulo_3', function() {
+        return function(input) {
+        	return input%3==1 ? 'margin_left_60' : '';
+        };
+    }
+);
+    
+Sudoku.filter('row_modulo_3', function() {
+        return function(input) {
+        	return (input%3==0 && input<9) ? 'margin_bottom_60' : '';
+        };
+    }
+);
+    
+Sudoku.filter('check_number', function() {
+        return function(input) {
+        	if (input >= 1 && input <= 9)    		
+        		return input;
+        };
+    }
+);
+
+Sudoku.filter('column_modulo_3_style', function(){
+        return function(input) {
+            return (input%3==1) ? 'padding-left:3px;' : '';
+        };
+    }
+);
+
+Sudoku.filter('row_modulo_3_style', function(){
+        return function(input) {
+            return (input%3==0 ) ? 'padding-bottom:3px;' : '';
+        };
+    }
+);
+
