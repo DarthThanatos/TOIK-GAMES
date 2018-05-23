4e459e85d99bcf06ddfdbf351bbb1f947caaef0b
Robert <bielas.robert95@gmail.com>
2018-05-24 00:30:00 +0200

    memo configuring init

--- memo/js/app-memo.js
+++ memo/js/app-memo.js
@@ -8,6 +8,8 @@ var memoryGameApp = angular.module('memoryGameApp', []);
 memoryGameApp.factory('game', function() {
   var tileNames = ['8-ball', 'kronos', 'baked-potato', 'dinosaur', 'rocket', 'skinny-unicorn',
     'that-guy', 'zeppelin'];
+  
+  console.log("Creating new memo game with session data: " + window.name);  
 
   return new Game(tileNames);
 });
